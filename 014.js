557cf9462da8e5d7d0e53589560a38d414abd2ae
Hammad Afzal <hammad.afzal.code@gmail.com>
2024-01-01 19:33:28 +0500

    first class functions && other info

---
+++ 014.js
@@ -0,0 +1,29 @@
+// function statement
+function a(){
+    console.log(100)
+}
+// function Expression
+let a = function(){
+    console.log(200)
+}
+// function Declaration
+
+// Anonymous function 
+// function(){
+//     console.log(300) // but it will say function statement requires a name ecma standard // it is used in a place where functionsare used as a value
+// }
+// Named function expression
+var c = function xyz(){
+    console.log(400)
+}
+// Difference between parameters & arguments ?
+
+// first class function 
+
+// we can return a function from a function, put function as a argument etc that's why functions are called firdt class citizens
+
+// Arrow functions 
+
+const y =()=>{
+    
+}
